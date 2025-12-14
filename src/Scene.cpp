28750fb46523675d2fc35fff3c56fa78faa13dde
Matt Stepita <mattomg24@gmail.com>
2025-12-14 09:56:39 -0800

    feat(src):  Add abstract scene class

---
+++ src/Scene.cpp
@@ -0,0 +1,9 @@
+#include "Scene.h"
+
+void Scene::registerAction(sf::Keyboard::Scancode code, std::string name){
+	m_inputMap[code] = name;
+}
+
+const InputMap& Scene::getInputMap() const {
+	return m_inputMap;
+}
---
+++ src/Scene.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <string>
+#include <unordered_map>
+#include "EntityManager.h"
+#include "Action.h"
+
+typedef std::unordered_map<sf::Keyboard::Scancode, std::string> InputMap;
+
+class Scene {
+	EntityManager m_entityManager;
+	size_t m_currentFrame = 0;
+	InputMap m_inputMap;
+	bool m_paused = false;
+	bool m_hasEnded = false;
+
+public:
+	virtual void update() = 0;
+	virtual void sDoAction(Action& action) = 0;
+	virtual void sRender(sf::RenderWindow& window) = 0;
+
+	void registerAction(sf::Keyboard::Scancode code, std::string name);
+	
+	const InputMap& getInputMap() const;
+
+};
